53d90147468ee71296ad24a56aaef66cf484c214
14_HUNG_0148 <148058664+14-Hung-17A3@users.noreply.github.com>
2024-01-09 22:26:38 +0700

    Add files via upload

---
+++ chương 13/tất cả bài của chương 13.py
@@ -0,0 +1,14 @@
+#bài 101
+with open("HumptyDumty.txt","w",encoding='utf-8') as f:
+    f.write("Nội dung tập tin:\n")
+    f.write("Humpty Dumpty sat on a wall,\n")
+    f.write("Humpty Dumpty had a great fall \n")
+    f.write("All the king's horses and all the king's men\n")
+    f.write("Couldn't put Humpty together again")
+f.close()
+    
+#bài 103
+with open("Rain.txt","w",encoding='utf-8') as f:
+    f.write("Rain rain, go away; Come again another day....:\n")
+   
+f.close()
